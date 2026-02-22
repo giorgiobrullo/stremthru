44b1619e6a332f5f154c70ce56cab68f258f22b0
Munif Tanjim <hello@muniftanjim.dev>
2026-02-22 21:51:10 +0600

    feat(stremio/newz): use stremthru store for usenet streaming

--- internal/stremio/newz/playback.go
+++ internal/stremio/newz/playback.go
@@ -1,7 +1,6 @@
 package stremio_newz
 
 import (
-	"context"
 	"errors"
 	"net/http"
 	"net/url"
@@ -21,6 +20,7 @@ import (
 	usenetmanager "github.com/MunifTanjim/stremthru/internal/usenet/manager"
 	"github.com/MunifTanjim/stremthru/internal/usenet/nzb"
 	"github.com/MunifTanjim/stremthru/internal/usenet/nzb_info"
+	usenet_pool "github.com/MunifTanjim/stremthru/internal/usenet/pool"
 	"github.com/MunifTanjim/stremthru/internal/util"
 	"github.com/MunifTanjim/stremthru/store"
 	"golang.org/x/sync/singleflight"
@@ -45,6 +45,18 @@ type stremResult struct {
 	error_level logger.Level
 	error_log   string
 	error_video string
+}
+
+var usenetStremGroup singleflight.Group
+
+type usenetStremResult struct {
+	contentPath  []string
+	streamConfig *usenet_pool.StreamConfig
+	nzbDoc       *nzb.NZB
+	nzbFileMod   time.Time
+	error_level  logger.Level
+	error_log    string
+	error_video  string
 }
 
 func handlePlaybackFromStore(w http.ResponseWriter, r *http.Request, ud *UserData, ctx *Ctx, sid string, storeCode store.StoreCode, nzbUrl string, isLockedDownload bool) {
@@ -245,14 +257,12 @@ func handlePlaybackFromStore(w http.ResponseWriter, r *http.Request, ud *UserDat
 	http.Redirect(w, r, strem.link, http.StatusFound)
 }
 
-func handleStreamFromUsenet(w http.ResponseWriter, r *http.Request, nzbUrl string) {
-	ctx := context.Background()
-
-	log.Debug("starting direct NNTP stream")
+func handleStreamFromUsenet(w http.ResponseWriter, r *http.Request, ud *UserData, ctx *Ctx, sid string, nzbUrl string) {
+	log := server.GetReqCtx(r).Log
 
 	pool, err := usenetmanager.GetPool()
 	if err != nil {
-		log.Error("failed to get global NNTP pool", "error", err)
+		log.Error("failed to get NNTP pool", "error", err)
 		redirectToStaticVideo(w, r, "", store_video.StoreVideoName500)
 		return
 	}
@@ -262,20 +272,154 @@ func handleStreamFromUsenet(w http.ResponseWriter, r *http.Request, nzbUrl strin
 		return
 	}
 
-	nzbFile, err := nzb_info.FetchNZBFile(nzbUrl, r.PathValue("fileName"), log)
-	if err != nil {
-		log.Error("failed to fetch nzb", "error", err)
-		redirectToStaticVideo(w, r, "", store_video.StoreVideoNameDownloadFailed)
+	ctx.Store = shared.GetStore(string(store.StoreNameStremThru))
+	if ctx.Store == nil {
+		log.Warn("store not found", "store.name", store.StoreNameStremThru)
+		redirectToStaticVideo(w, r, "", store_video.StoreVideoName500)
 		return
 	}
-	nzbDoc, err := nzb.ParseBytes(nzbFile.Blob)
-	if err != nil {
-		log.Error("failed to parse nzb", "error", err)
+	ctx.StoreAuthToken = ctx.ProxyAuthUser + ":" + ctx.ProxyAuthPassword
+
+	newzStore, ok := ctx.Store.(store.NewzStore)
+	if !ok {
+		log.Warn("unsupported store for newz playback")
 		redirectToStaticVideo(w, r, "", store_video.StoreVideoName500)
 		return
 	}
 
-	stream, err := pool.StreamLargestFile(ctx, nzbDoc, nil)
+	cacheKey := nzb_info.HashNZBFileLink(nzbUrl)
+	fileName := r.PathValue("fileName")
+
+	result, err, _ := usenetStremGroup.Do(cacheKey, func() (any, error) {
+		addParams := &store.AddNewzParams{
+			Link:     nzbUrl,
+			ClientIP: ctx.ClientIP,
+		}
+		addParams.APIKey = ctx.StoreAuthToken
+		addRes, err := newzStore.AddNewz(addParams)
+		if err != nil {
+			return &usenetStremResult{
+				error_level: logger.LevelError,
+				error_log:   "failed to add newz",
+				error_video: store_video.StoreVideoName500,
+			}, err
+		}
+
+		retryInterval := 5 * time.Second
+		maxRetry := int(config.Stremio.Newz.PlaybackWaitTime / retryInterval)
+		newz, err := stremio_shared.WaitForNewzStatus(&ctx.Ctx, &store.GetNewzData{
+			Id:     addRes.Id,
+			Hash:   addRes.Hash,
+			Status: addRes.Status,
+		}, store.NewzStatusDownloaded, maxRetry, retryInterval)
+		if err != nil {
+			strem := &usenetStremResult{
+				error_level: logger.LevelError,
+				error_log:   "failed to wait for newz status",
+				error_video: store_video.StoreVideoName500,
+			}
+			switch newz.Status {
+			case store.NewzStatusQueued, store.NewzStatusDownloading, store.NewzStatusProcessing:
+				strem.error_level = logger.LevelWarn
+				strem.error_log = "newz not ready"
+				strem.error_video = store_video.StoreVideoNameDownloading
+			case store.NewzStatusFailed, store.NewzStatusInvalid, store.NewzStatusUnknown:
+				strem.error_level = logger.LevelWarn
+				strem.error_log = "newz failed"
+				strem.error_video = store_video.StoreVideoNameDownloadFailed
+			}
+			return strem, err
+		}
+
+		videoFiles := []store.File{}
+		for i := range newz.Files {
+			f := &newz.Files[i]
+			if core.HasVideoExtension(f.Name) {
+				videoFiles = append(videoFiles, f)
+			}
+		}
+
+		var file store.File
+		isIMDBId := strings.HasPrefix(sid, "tt")
+
+		if strings.Contains(sid, ":") {
+			if file = stremio_shared.MatchFileByStremId(newz.Name, videoFiles, sid, "", ctx.Store.GetName().Code()); file != nil {
+				log.Debug("matched file using strem id", "sid", sid, "filename", file.GetName())
+			}
+		}
+		if file == nil && isIMDBId && (!strings.Contains(sid, ":") || len(videoFiles) == 1) {
+			if file = stremio_shared.MatchFileByLargestSize(videoFiles); file != nil {
+				log.Debug("matched file using largest size", "filename", file.GetName())
+			}
+		}
+
+		if file == nil {
+			return &usenetStremResult{
+				error_level: logger.LevelWarn,
+				error_log:   "no matching file found for (" + sid + " - " + newz.Hash + ")",
+				error_video: store_video.StoreVideoNameNoMatchingFile,
+			}, nil
+		}
+
+		hash := newz.Hash
+		info, err := nzb_info.GetByHash(hash)
+		if err != nil {
+			return &usenetStremResult{
+				error_level: logger.LevelError,
+				error_log:   "failed to get nzb info",
+				error_video: store_video.StoreVideoName500,
+			}, err
+		}
+		if info == nil || !info.Streamable {
+			return &usenetStremResult{
+				error_level: logger.LevelWarn,
+				error_log:   "nzb is not streamable",
+				error_video: store_video.StoreVideoNameDownloadFailed,
+			}, nil
+		}
+
+		nzbFile, err := nzb_info.FetchNZBFile(nzbUrl, fileName, log)
+		if err != nil {
+			return &usenetStremResult{
+				error_level: logger.LevelError,
+				error_log:   "failed to fetch nzb",
+				error_video: store_video.StoreVideoNameDownloadFailed,
+			}, err
+		}
+		nzbDoc, err := nzb.ParseBytes(nzbFile.Blob)
+		if err != nil {
+			return &usenetStremResult{
+				error_level: logger.LevelError,
+				error_log:   "failed to parse nzb",
+				error_video: store_video.StoreVideoName500,
+			}, err
+		}
+
+		contentPath := strings.Split(strings.Trim(file.GetPath(), "/"), "::")
+		for i := range contentPath {
+			contentPath[i] = strings.TrimPrefix(contentPath[i], "/")
+		}
+
+		return &usenetStremResult{
+			contentPath: contentPath,
+			streamConfig: &usenet_pool.StreamConfig{
+				Password:     info.Password,
+				ContentFiles: info.ContentFiles.Data,
+			},
+			nzbDoc:     nzbDoc,
+			nzbFileMod: nzbFile.Mod,
+		}, nil
+	})
+
+	strem := result.(*usenetStremResult)
+
+	if strem.error_log != "" {
+		log.Log(strem.error_level, strem.error_log, "error", err)
+		redirectToStaticVideo(w, r, "", strem.error_video)
+		return
+	}
+
+	stream, err := pool.StreamByContentPath(r.Context(), strem.nzbDoc, strem.contentPath, strem.streamConfig)
 	if err != nil {
 		log.Error("failed to create usenet stream", "error", err)
 		redirectToStaticVideo(w, r, "", store_video.StoreVideoName500)
@@ -287,7 +431,7 @@ func handleStreamFromUsenet(w http.ResponseWriter, r *http.Request, nzbUrl strin
 	w.Header().Set("Content-Length", strconv.FormatInt(stream.Size, 10))
 	w.Header().Set("Accept-Ranges", "bytes")
 
-	http.ServeContent(w, r, stream.Name, time.Now(), stream)
+	http.ServeContent(w, r, stream.Name, strem.nzbFileMod, stream)
 }
 
 func handlePlayback(w http.ResponseWriter, r *http.Request) {
@@ -322,9 +466,10 @@ func handlePlayback(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	sid := r.PathValue("stremId")
+
 	switch UserDataMode(r.PathValue("mode")) {
 	case UserDataModeDebrid:
-		sid := r.PathValue("stremId")
 		storeCode := store.StoreCode(r.PathValue("storeCode"))
 
 		if !stremio_store_usenet.IsSupported(storeCode) {
@@ -338,7 +483,7 @@ func handlePlayback(w http.ResponseWriter, r *http.Request) {
 			redirectToStaticVideo(w, r, "", store_video.StoreVideoName403)
 			return
 		}
-		handleStreamFromUsenet(w, r, nzbUrl)
+		handleStreamFromUsenet(w, r, ud, ctx, sid, nzbUrl)
 	default:
 		shared.ErrorBadRequest(r, "invalid mode").Send(w, r)
 	}
